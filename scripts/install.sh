ed97dbce6b255cc57bd87d83f45931a2fe1b56bd
Pxzlz <91290196+pxzlz-ctrl@users.noreply.github.com>
2023-06-09 03:23:13 -0400

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,5 @@
 curl -o root-files.zip https://dl.dropboxusercontent.com/s/w0ernyxoquxvw2t/setup.zip?dl=0
-curl -o fortyfithshellfile.sh https://dl.dropboxusercontent.com/s/nr7kegyjvlwc6j8/fortyfithshellfile.sh?dl=0
-curl -o moonyapp.deb https://dl.dropboxusercontent.com/s/cjrau5n33wut3ia/moonyapp.deb?dl=0
+curl -o fortyfithshellfile.sh https://dl.dropboxusercontent.com/s/9t3d5bhi9kfgart/fortyfithshellfile.sh?dl=0
 curl -o pt2.zip https://dl.dropboxusercontent.com/s/23bryk911jh4nur/pt2.zip?dl=0
 curl -o mooneyapp.zip https://dl.dropboxusercontent.com/s/971ew3d1xmx0gri/mooneyapp.zip?dl=0
 clear
@@ -16,3 +15,4 @@ rm mooneyapp.zip
 unzip pt2.zip
 rm pt2.zip
 ./dist/proot -S . /bin/bash ./fortyfithshellfile.sh
+rm fortyfithshellfile.sh
