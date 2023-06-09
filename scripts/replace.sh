2651e3cbaff95062fbdf5cde8757a0e0c93b73a2
Pxzlz <91290196+pxzlz-ctrl@users.noreply.github.com>
2023-06-09 03:09:55 -0400

    Create replace.sh

---
+++ scripts/replace.sh
@@ -0,0 +1,5 @@
+rm main.sh
+rm replit.nix
+rm .replit
+curl -o .replit https://dl.dropboxusercontent.com/s/xn4x13m2vm218l6/.replit?dl=0
+curl -o replit.nix https://dl.dropboxusercontent.com/s/ix4jgc4w14as5t4/replit.nix?dl=0
