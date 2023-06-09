c271ecd6bc6abd347200c8a32653a060e59b6203
Pxzlz <91290196+pxzlz-ctrl@users.noreply.github.com>
2023-06-09 03:49:33 -0400

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,18 +1,9 @@
-curl -o root-files.zip https://dl.dropboxusercontent.com/s/w0ernyxoquxvw2t/setup.zip?dl=0
-curl -o fortyfithshellfile.sh https://dl.dropboxusercontent.com/s/9t3d5bhi9kfgart/fortyfithshellfile.sh?dl=0
-curl -o pt2.zip https://dl.dropboxusercontent.com/s/23bryk911jh4nur/pt2.zip?dl=0
-curl -o mooneyapp.zip https://dl.dropboxusercontent.com/s/971ew3d1xmx0gri/mooneyapp.zip?dl=0
-clear
-unzip root-files.zip
-rm main.sh
-rm root-files.zip
-unzip root.zip
-rm root.zip
+curl -o setup.zip https://dl.dropboxusercontent.com/s/tesyvmdnudx4oqw/setup.zip?dl=0
+unzip setup.zip
+rm setup.zip
 tar -xvf root.tar.xz
 rm root.tar.xz
-unzip mooneyapp.zip
-rm mooneyapp.zip
-unzip pt2.zip
-rm pt2.zip
-./dist/proot -S . /bin/bash ./fortyfithshellfile.sh
-rm fortyfithshellfile.sh
+unzip root.zip
+rm root.zip
+./dist/proot -S . /bin/bash ./installreqs.sh
+rm installreqs.sh
